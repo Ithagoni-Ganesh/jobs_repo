a229bc6dac6a090ba2a557c52194843ccddbb823
ITHAGONI GANESH <ithagoni.ganesh@gmail.com>
2024-04-02 13:35:19 +0530

    first commit

---
+++ sunnyjobs/testapp/populate.py
@@ -0,0 +1,8 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','sunnyjobs.settings')
+import django
+django.setup()
+
+from faker import Faker
+from testapp.models import Student
+from random import
